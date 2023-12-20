d91445928cf4fe9128e8bc17e4b2aa1414337779
pang <pangton28.p@gmail.com>
2023-12-20 16:39:07 +0700

    feat(exercise): add complete styling

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,42 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 
-function Button() {}
+function Button(props) {
+    if (props.type === 'primary') {
+        return(
+            <div>
+            <button css={css`
+              width: 171.19px;
+              height: 50px;
+              border-radius: 4px;
+              padding: 0px, 16px;
+              background-color: #074EE8;
+              color: white;
+
+            `}
+            >
+             {props.text}
+            </button>
+            </div>
+        )
+    } else if (props.type === 'secondary') {
+        return(
+            <button css={css`
+              width: 171.19px;
+              height: 50px;
+              border-radius: 4px;
+              padding: 0px, 16px;
+              background-color: #07A4E8;
+              color: white;
+              margin-top: 20px;
+            `}
+            >
+             {props.text}
+            </button>
+        )
+    }
+    
+}
+
+export default Button
